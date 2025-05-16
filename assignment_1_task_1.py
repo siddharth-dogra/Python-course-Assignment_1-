b2ae7ebd161aa6600a6aa558dc720659d0415a5c
SIDDHARTH DOGRA <93005001+siddharth-dogra@users.noreply.github.com>
2025-05-16 22:48:15 +0530

    Add files via upload

---
+++ assignment_1_task_1.py
@@ -0,0 +1,10 @@
+first=int(input("Enter the first number: "))
+second = int(input("Enter the second number: "))
+addition=first+second
+subtraction=first-second
+multiplication=first*second
+division=first/second
+print("addition: ",addition)
+print("subtraction: ",subtraction)
+print("multiplication: ",multiplication)
+print("division: ",division)
---
+++ assignment_1_task_2.py
@@ -0,0 +1,4 @@
+first=input("Enter your first name: ")
+last=input("Enter your last name: ")
+result=first+" "+ last
+print("hello, " + result +  "! welcome to the python program.")
